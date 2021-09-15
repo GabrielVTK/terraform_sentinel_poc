3bc1b8f324754d3e6c0212b18ac40864f38b22d9
Gabriel Vinicius <gabriel.vinicius.dev@gmail.com>
2021-09-15 09:57:18 -0300

    add mandatory tag

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "webserver" {
   ami = "ami-09e67e426f25ce0d7" # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type - ami-09e67e426f25ce0d7 (64 bits x86)
   instance_type = "t2.micro"
   tags = {
-    "Name" = "SENTINEL_POC"
+    "Name" = "SENTINEL_POC",
+    "Departamento" = "Arq. Tecnologia"
   }
 }

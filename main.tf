9621d7c133b7607b6863a2af4c5683af8fead95b
Gabriel Vinicius <gabriel.vinicius.dev@gmail.com>
2021-09-17 10:42:54 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "webserver" {
   ami = "ami-09e67e426f25ce0d7" # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type - ami-09e67e426f25ce0d7 (64 bits x86)
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   tags = {
     "Name" = "SENTINEL_POC",
     "Department" = "Arq. Tecnologia"
